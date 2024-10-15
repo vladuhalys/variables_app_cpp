64c9102f6bc2f5906a1f0b6ea890ff550e656b72
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2024-10-15 15:15:51 +0300

    Task 1 from Homework was Done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	int x = 10;
+	int y = 20;
+	int z = x + y;
+	cout << "The sum of " << x << " and " << y << " is " << z << endl;
 	return 0;
 }
